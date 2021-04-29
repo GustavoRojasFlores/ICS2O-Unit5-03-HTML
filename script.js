b4db4d9faf5fbcf4bdc6463e7b260877557b1e84
Gustavo Rojas <56005777+GustavoRojasFlores@users.noreply.github.com>
2021-04-29 09:39:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ document.getElementById('btn').addEventListener('click', onButton)
 
 function onButton () {
   alert(document.getElementById('txt').value) // Displays a message that matches the text in the input box.
-} 
+}
