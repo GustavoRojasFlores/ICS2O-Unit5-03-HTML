b33c0da9128b5ec0c79ce28a35c7b2871cf46b19
Gustavo Rojas <>
2021-04-29 13:22:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').addEventListener('click', onButton)
+
+function onButton () {
+  alert(document.getElementById('txt').value) // Displays a message that matches the text in the input box.
+} 
